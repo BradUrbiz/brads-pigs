b94b0703496a854afb95692ddcb3f788f0205dfa
BradUrbiz <bradurbiz@gmail.com>
2026-02-03 16:12:11 -0500

    Detect when clicking r on keyboard

--- src/main/java/com/bradurbiztondo/bradspigs/BradsPigsClient.java
+++ src/main/java/com/bradurbiztondo/bradspigs/BradsPigsClient.java
@@ -6,11 +6,35 @@ import com.bradurbiztondo.bradspigs.registry.ModEntities;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.text.Text;
+
+// using https://wiki.fabricmc.net/tutorial:keybinds
+// yarn equivalents instead of mojang
+
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import org.lwjgl.glfw.GLFW;
 
 public class BradsPigsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.register(ModEntities.BABOY, BaboyRenderer::new);
         EntityModelLayerRegistry.registerModelLayer(BaboyModel.LAYER_LOCATION, BaboyModel::getTexturedModelData);
+
+        KeyBinding throwTntKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.bradspigs.throw_tnt",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_F,
+                "category.bradspigs.controls"));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player == null) return;
+
+            while (throwTntKey.wasPressed()) {
+                client.player.sendMessage(Text.literal("Clicked R"), false);
+            }
+        });
     }
 }
